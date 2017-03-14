b9d97f0121a4dfafbec296c45b3a177aa4fe5dfa
zyl <528846308@qq.com>
2017-03-14 11:37:52 +0800

    发现页面搭建

--- YiHeYun/Classes/发现/FindVC.h
+++ YiHeYun/Classes/发现/FindVC.h
@@ -7,7 +7,19 @@
 //
 
 #import "BaseVC.h"
+#import "SDCycleScrollView.h"
 
-@interface FindVC : BaseVC
+@interface FindCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *cellImgView;
+@property (weak, nonatomic) IBOutlet UILabel *cellTitleLB;
+@property (weak, nonatomic) IBOutlet UILabel *cellTimeLB;
+@property (weak, nonatomic) IBOutlet UIView *cellBgView;
+
+@end
+
+
+@interface FindVC : BaseVC<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet SDCycleScrollView *topBannerView;
+@property (weak, nonatomic) IBOutlet UITableView *findTabView;
 
 @end
--- YiHeYun/Classes/发现/FindVC.m
+++ YiHeYun/Classes/发现/FindVC.m
@@ -8,6 +8,16 @@
 
 #import "FindVC.h"
 
+@implementation FindCell
+
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    _cellBgView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    _cellBgView.layer.borderWidth = 0.5;
+}
+
+@end
+
 @interface FindVC ()
 
 @end
@@ -16,8 +26,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+self.title = @"发现";
+    UIView *bgView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, WIDTH,  WIDTH/2.0+20)];
+    _topBannerView.frame = CGRectMake(15, 10, WIDTH-30, WIDTH/2.0);
+    _topBannerView.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    [bgView addSubview:_topBannerView];
+    _topBannerView.imageURLStringsGroup = @[@"http://pic.58pic.com/58pic/14/15/82/29S58PICTPg_1024.jpg",@"http://pic27.nipic.com/20130319/11935511_225831392000_2.jpg"];
+    _topBannerView.pageControlAliment = SDCycleScrollViewPageContolAlimentRight;
+    _findTabView.tableHeaderView = bgView;
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    FindCell *cell = [tableView dequeueReusableCellWithIdentifier:@"findCell"];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 30;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, WIDTH, 40)];
+    UILabel *ll = [[UILabel alloc]initWithFrame:CGRectMake(15, 10, WIDTH-30, 20)];
+    ll.textColor = [UIColor darkGrayColor];
+    ll.text = @"活动";
+    ll.font = [UIFont systemFontOfSize:14];
+    [view addSubview:ll];
+
+    return view;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
