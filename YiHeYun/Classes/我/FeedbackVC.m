9835e4a1351246d091ebee15c5979ba5e22a70e3
zengXianShu <zengxiangshu0@163.com>
2017-03-29 12:10:01 +0800

    update FeedbackVC ( UI is ok)

--- YiHeYun/Classes/我/FeedbackVC.m
+++ YiHeYun/Classes/我/FeedbackVC.m
@@ -10,28 +10,70 @@
 
 @interface FeedbackVC ()
 
+@property (weak, nonatomic) IBOutlet UITextView *feedBackTextView;
+@property (weak, nonatomic) IBOutlet UILabel *feedBackPlaceholderLabel;
+
+@property (weak, nonatomic) IBOutlet UITextView *contactTextView;
+@property (weak, nonatomic) IBOutlet UILabel *contactPlaceholderLabel;
+
+
+
 @end
 
 @implementation FeedbackVC
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-  self.title = @"留言";
+  [super viewDidLoad];
+  self.title = @"意见反馈";
+
+  [self configUI];
 }
+
+- (void)configUI {
+  self.view.backgroundColor = [UIColor colorWithWhite:0.928 alpha:1.0];
+
+  self.feedBackTextView.delegate = self;
+  self.feedBackTextView.textContainerInset = UIEdgeInsetsMake(10, 13, 10, 13);
+
+  self.contactTextView.delegate = self;
+  self.contactTextView.textContainerInset = UIEdgeInsetsMake(17, 13, 17, 13);
+}
+
+- (IBAction)CommitAction {
+  NSString *feedBackStr = self.feedBackTextView.text;
+  NSString *contactStr = self.contactTextView.text;
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+
+  if (textView == self.feedBackTextView) {
+    if (![text isEqualToString:@""]) {
+      self.feedBackPlaceholderLabel.hidden = true;
+    }
+
+    if ([text isEqualToString:@""] && range.location == 0 && range.length == 1) {
+      self.feedBackPlaceholderLabel.hidden = false;
+    }
+  }
+
+  if (textView == self.contactTextView) {
+    if (![text isEqualToString:@""]) {
+      self.contactPlaceholderLabel.hidden = true;
+    }
+
+    if ([text isEqualToString:@""] && range.location == 0 && range.length == 1) {
+      self.contactPlaceholderLabel.hidden = false;
+    }
+  }
+
+  return YES;
+}
+
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
